d00749c0a1403fd8e7194b49190789402be531db
10969-ALAMGIRKA\Roni <alamgir.roni@bjitgroup.com>
2021-08-21 15:31:55 +0600

    project code

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ['links.papareact.com']
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiYWxhbWdpci1yb25pIiwiYSI6ImNrc2pxODRzdzJnNDUydm8yM2xtYzl1dGoifQ.FvAetDIVj-g37J4vHYe6Fw'
+    }
+}
